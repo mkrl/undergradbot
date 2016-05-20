3b36e4289841c30b83d6067f447f58d5a7968a0c
Guy <stranger@vds2546.myarena.ru>
2016-05-20 04:14:49 -0400

    major update, now registration is finally working and dbs are handling stuff pretty damn good

--- bot.py
+++ bot.py
@@ -4,21 +4,48 @@ import telebot
 import dbaser
 import random
 import tools
-import re
 import shelve
+from cfg import slocal
+from cfg import glocal
 
-reg=1
+
+
 bot = telebot.TeleBot(cfg.token)
 vy = ['Chto za vopros? Vy yaz!', 'Samyj lubimyi yaz', 'Nash yaz', 'Kot', 'Krot', 'Superkrot konechno!', 'Yaz', 'Milyj yaz', 'Yedinstvennyj yaz']
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Hello there. I'm a brainless bot. Please tell me your group number.")
+#    dbaser.cur.execute('SELECT schedule.stime, schedule.room, lessons.lessname, lessons.teachname FROM schedule INNER JOIN lessons WHERE  schedule.lessid=lessons.lessid AND schedule.gid=2321 ORDER BY schedule.stime ASC')
+
+@bot.message_handler(commands=['getme']) #building a new client
+def send_welcome(message):
+    bot.reply_to(message, "Filling in a database...")
+    s = shelve.open(slocal)
+    s[str(message.chat.id)] = 0
+    wat = s[str(message.chat.id)]
+    print(wat)
+    bot.send_message(message.chat.id, "Got it, pal! Your ID is "+str(message.chat.id)+"And your status is ")    
+    s.close()
+    
+#GENERAL STATUS CHECK SQLS
+# UPDATE GROUP set gid=X where client = chat.id
+# UPDATE STATUS set status=X where client = chat.id
+# SELECT status FROM STATUS where  client = chat.id
+# SELECT gid FROM GROUP where  client = chat.id
+# INSERT INTO STATUS (client,status) \
+#      VALUES (chat.id, x)
+
+
 
 @bot.message_handler(commands=['register'])
 def register(message):
   bot.send_message(message.chat.id, "Please state your group number (looks something like 2xxx).")
-
+  s = shelve.open(slocal)
+  s[str(message.chat.id)] = 1
+  ass = s[str(message.chat.id)]
+  print(ass)
+  
 #  match = re.match('2[1-5][1-5][1-5]', group, flags=0)
 #  if len(match)>0 :
 #        bot.send_message(message.chat.id, "Invalid group number, please register again.")
@@ -27,13 +54,21 @@ def register(message):
 #  storage[str(chat_id)] = group
 #  bot.send_message(message.chat.id, "Group updated successfully. Yours is "+group)
 
-#@bot.message_handler(regexp="2[1-5][1-5][1-5]")
-#def handle_message(message):
-#  if reg == 1:
-#    group = message.text
-#    with shelve.open(cfg.slocal) as storage:
-#      storage[str(chat_id)] = group
-#    bot.send_message(message.chat.id, "Group updated successfully. Yours is "+group)
+@bot.message_handler(regexp="2[1-5][1-5][1-5]")
+def handle_message(message):
+  s = shelve.open(slocal)
+  if  s[str(message.chat.id)] == 1:
+    group = ''
+    gs = shelve.open(glocal)
+    group = message.text
+    gs[str(message.chat.id)] = group
+    bot.send_message(message.chat.id, "Group updated successfully. Yours is "+group)
+    gs.close()
+  s[str(message.chat.id)] = 2 
+  pens =  s[str(message.chat.id)]
+  print(pens)
+  s.close()
+
 
 @bot.message_handler(commands=['ktoya'])
 def yaz(message):
--- getstuff.py
+++ getstuff.py
@@ -1,15 +1,33 @@
-import dbaser
+
+import shelve
+
+
+s = shelve.open('deeznuts')
+try:
+    s['clid']=0
+finally:
+    s.close()
+
+s = shelve.open('deeznuts.db')
+try:
+    fuck = s['clid']
+finally:
+    s.close()
+
+print(fuck)
+
+
 
 #dont forget to close cur with dbaser.cur.close!!
 
-
-dbaser.cur.execute('SELECT schedule.stime, schedule.room, lessons.lessname, lessons.teachname FROM schedule INNER JOIN lessons WHERE  schedule.lessid=lessons.lessid AND schedule.gid=2321 ORDER BY schedule.stime ASC')
-text = ''
-for row in dbaser.cur:
-   temp = ("At " + str(row[0]) + " in room no. " + str(row[1]) + "\n takes place " + str(row[2]) + ". \nThe teacher name is " + str(row[3]) + "\n \n")
-   text = text + temp
-print text;
-dbaser.con.close()
+#
+#dbaser.cur.execute('SELECT schedule.stime, schedule.room, lessons.lessname, lessons.teachname FROM schedule INNER JOIN lessons WHERE  schedule.lessid=lessons.lessid AND schedule.gid=2321 ORDER BY schedule.stime ASC')
+#text = ''
+#for row in dbaser.cur:
+#   temp = ("At " + str(row[0]) + " in room no. " + str(row[1]) + "\n takes place " + str(row[2]) + ". \nThe teacher name is " + str(row[3]) + "\n \n")
+#   text = text + temp
+#print text;
+#dbaser.con.close()
 
 #    dbaser.cur.execute('SELECT schedule.stime, schedule.room, lessons.lessname, lessons.teachname FROM schedule INNER JOIN lessons WHERE  schedule.lessid=lessons.lessid AND schedule.gid=2321 ORDER BY schedule.stime ASC')
 #    for row in dbaser.cur:
