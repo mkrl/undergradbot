4a503247e426c1a07f3bf125cd441a4455ccda08
Guy <stranger@vds2546.myarena.ru>
2016-05-20 12:19:28 -0400

    a stable working wersion with basic functionallity

--- bot.py
+++ bot.py
@@ -13,20 +13,19 @@ from cfg import glocal
 bot = telebot.TeleBot(cfg.token)
 vy = ['Chto za vopros? Vy yaz!', 'Samyj lubimyi yaz', 'Nash yaz', 'Kot', 'Krot', 'Superkrot konechno!', 'Yaz', 'Milyj yaz', 'Yedinstvennyj yaz']
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Hello there. I'm a brainless bot. Please tell me your group number.")
-#    dbaser.cur.execute('SELECT schedule.stime, schedule.room, lessons.lessname, lessons.teachname FROM schedule INNER JOIN lessons WHERE  schedule.lessid=lessons.lessid AND schedule.gid=2321 ORDER BY schedule.stime ASC')
-
-@bot.message_handler(commands=['getme']) #building a new client
+@bot.message_handler(commands=['start']) #building a new client
 def send_welcome(message):
     bot.reply_to(message, "Filling in a database...")
     s = shelve.open(slocal)
     s[str(message.chat.id)] = 0
     wat = s[str(message.chat.id)]
     print(wat)
-    bot.send_message(message.chat.id, "Got it, pal! Your ID is "+str(message.chat.id)+"And your status is ")    
+    bot.send_message(message.chat.id, 'Got it, pal! Your ID is '+str(message.chat.id)+'. Now lets get it started.')    
     s.close()
+
+@bot.message_handler(commands=['help']) #general help
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Hi! They call me IMEI bot and I'm able to deliver your today's schedule here, tell you some usefull stuff and more! \n Commands: \n \n */help* - shows this page \n */register* - Tell me your group number \n */today* - Yout today's schedule and how much time do you have till a lesson starts. \n *???* ", parse_mode="Markdown")
     
 #GENERAL STATUS CHECK SQLS
 # UPDATE GROUP set gid=X where client = chat.id
@@ -40,7 +39,7 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['register'])
 def register(message):
-  bot.send_message(message.chat.id, "Please state your group number (looks something like 2xxx).")
+  bot.send_message(message.chat.id, "Please state your group number (looks something like 2xxx). Only available for 2321 for now though.")
   s = shelve.open(slocal)
   s[str(message.chat.id)] = 1
   ass = s[str(message.chat.id)]
@@ -84,12 +83,21 @@ def yaz(message):
 
 @bot.message_handler(commands=['today'])
 def give_today(message):
-    text=''
-    dbaser.cur.execute('SELECT schedule.stime, schedule.room, lessons.lessname, lessons.teachname FROM schedule INNER JOIN lessons WHERE  schedule.lessid=lessons.lessid AND schedule.gid=2321 ORDER BY schedule.stime ASC')
-    for row in dbaser.cur:
-        temp = tools.tohrs(row[0]) + ": Room no. " + str(row[1]) + "\n" + "*"+str(row[2])+"*" + ". \n" + str(row[3]) + "\n \n"
-        text = text + temp
-    bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    s = shelve.open(slocal)
+    if  s[str(message.chat.id)] == 1 or s[str(message.chat.id)] == 0:
+      bot.send_message(message.chat.id, "Please register first. Type /register to start.")
+      s.close()
+    else:     
+      text=''
+      gs = shelve.open(glocal)
+      grp = gs[str(message.chat.id)]
+      dbaser.cur.execute("SELECT schedule.stime, schedule.room, lessons.lessname, lessons.teachname FROM schedule INNER JOIN lessons WHERE  schedule.lessid=lessons.lessid AND schedule.gid=? ORDER BY schedule.stime ASC", (grp,))
+      for row in dbaser.cur:
+          temp = tools.tohrs(row[0]) + " *"+str(row[2])+"*" + "\n Room no. " + str(row[1]) + "\n" + str(row[3]) + "\n"
+          text = text + temp
+      bot.send_message(message.chat.id, text, parse_mode="Markdown")
+      gs.close()
+      s.close()
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
--- tools.py
+++ tools.py
@@ -1,24 +1,30 @@
-from telebot import types
-import shelve
 import dbaser
 import re
 
-#def register(chat_id):
-#  bot.send_message(message.chat.id, "Please state your group number (looks something like 2xxx).")
-#  group = message.text 
-#  match = re.match('2[1-5][1-5][1-5]', group, flags=0)
-#  if len(match)>0 :
-#        bot.send_message(message.chat.id, "Invalid group numberm please register again.")
-#  else : 
-#    with shelve.open(slocal) as storage:
-#          storage[str(chat_id)] = group
-#    bot.send_message(message.chat.id, "Group updated successfully.")
         
+        
+#def tohrs(seconds):              <= Worked in python 2.7
+#  z=seconds
+#  h=z/3600 
+#  m=z%3600/60 
+#  s=z%3600%60
+#  hr = str(h) + ":" + str(m)
+#  return hr
+#  
+  
         
 def tohrs(seconds):
-  z=seconds
-  h=z/3600 
-  m=z%3600/60 
-#  s=z%3600%60
-  hr = str(h) + ":" + str(m)
+  a=seconds
+  h=((a//3600))%24
+  m=(a//60)%60
+  s=a%60
+  if m<10:
+      m=str('0'+str(m))
+  else:
+      m=str(m)
+  if s<10:
+      s=str('0'+str(s))
+  else:
+      s=str(s)
+  hr=str(h)+':'+str(m)
   return hr
